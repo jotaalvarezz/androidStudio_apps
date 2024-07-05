b0bdce3cd58ee36eab590650db281e0ed2de69b8
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-05 16:43:49 -0500

    instancia de los componentes en controlador

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,18 +14,18 @@ import androidx.core.view.WindowInsetsCompat;
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-    public EditText et1;
-    public EditText etd2;
-    public Switch sw;
-    public TextView twr;
+    private EditText et1;
+    private EditText et2;
+    private Switch sw;
+    private TextView twr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-
+        et1 = (EditText)findViewById(R.id.value1);
+        et2 = (EditText)findViewById(R.id.value2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
