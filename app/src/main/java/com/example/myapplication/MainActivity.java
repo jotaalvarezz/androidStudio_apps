caa8e0f70be7979b0931f7df65184b2383e93ffa
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-05 16:32:07 -0500

    diseño de la calculadora

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,12 +14,18 @@ import androidx.core.view.WindowInsetsCompat;
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+    public EditText et1;
+    public EditText etd2;
+    public Switch sw;
+    public TextView twr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
