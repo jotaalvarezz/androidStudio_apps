327862f6d45e090d285ce47538364e9a6626d5a6
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-10 17:16:35 -0500

    imagebutton finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,22 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText edt1;
-    private EditText edt2;
-    private EditText edt3;
-    private EditText edt4;
-    private TextView tw1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        edt1 = (EditText) findViewById(R.id.text_value1);
-        edt2 = (EditText) findViewById(R.id.text_value2);
-        edt3 = (EditText) findViewById(R.id.text_value3);
-        edt4 = (EditText) findViewById(R.id.editTextText);
-        tw1 = (TextView) findViewById(R.id.textView6);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -37,28 +28,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void calculate(View view){
-        String value1 = edt1.getText().toString();
-        String value2 = edt2.getText().toString();
-        String value3 = edt3.getText().toString();
+    public void buttonMen(View view){
+        Toast.makeText(this, "Foro para Hombres", Toast.LENGTH_SHORT).show();
+    }
 
-        float prom = 0;
-
-        float note1 = Float.parseFloat(value1);
-        float note2 = Float.parseFloat(value2);
-        float note3 = Float.parseFloat(value3);
-
-        prom = (note1 + note2 + note3) / 3;
-
-        String res = "";
-        String promedio = String.valueOf(prom);
-        if(prom >= 6){
-            res = String.valueOf("aprobado");
-        }else if(prom < 6){
-            res = String.valueOf("reprobado");
-        }
-
-        tw1.setText(res);
-        edt4.setText(promedio);
+    public void buttonWomen(View view){
+        Toast.makeText(this, "Foro para Mujeres", Toast.LENGTH_SHORT).show();
     }
 }
