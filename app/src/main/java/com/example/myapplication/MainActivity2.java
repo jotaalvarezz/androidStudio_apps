e5c62abfc33af3222b088063c9cc9b504b4cff15
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-11 14:55:27 -0500

    actividad finalizada

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,9 +3,6 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.math.BigDecimal;
+public class MainActivity2 extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void next(View view){
-        Intent siguiente = new Intent(this, MainActivity2.class);
-        startActivity(siguiente);
+    public void back(View view){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
     }
 }
