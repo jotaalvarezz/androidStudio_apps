856bde96455510f2c6f6eb3f36421646013e7684
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-11 14:02:26 -0500

    finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        et1 = (EditText)findViewById(R.id.)
+        et1 = (EditText)findViewById(R.id.txt_user);
+        et2 = (EditText)findViewById(R.id.txt_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Login(View view){
+        String user = et1.getText().toString();
+        String pass = et2.getText().toString();
+
+        if(user.isEmpty() || user == null){
+            Toast.makeText(this, "¡Campo Usuario vacio!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(pass.isEmpty() || pass == null){
+            Toast.makeText(this, "¡Campo Contraseña vacio!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Toast.makeText(this,"Autenticacion en proceso...",Toast.LENGTH_SHORT).show();
     }
 }
