19eb1a6551dcb5448b8d18c288e30b96cccc5032
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-12 16:19:28 -0500

    finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ import androidx.core.view.WindowInsetsCompat;
 import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText et1;
+    private EditText et1, etm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         et1 = (EditText) findViewById(R.id.text_value1);
-        SharedPreferences preferences = getSharedPreferences("dato", Context.MODE_PRIVATE);
-        et1.setText(preferences.getString("mail", ""));
+        etm = (EditText) findViewById(R.id.text_mul);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -36,10 +35,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveChared(View view){
+        String name = et1.getText().toString();
+        String datos = etm.getText().toString();
+
         SharedPreferences preferens = getSharedPreferences("dato", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferens.edit();
-        editor.putString("mail",et1.getText().toString());
+        editor.putString(name, datos);
         editor.commit();
         finish();
+    }
+
+    public void searh(View view){
+        String name = et1.getText().toString();
+        SharedPreferences preferences = getSharedPreferences("dato", Context.MODE_PRIVATE);
+        String datos = preferences.getString(name, "");
+
+        if(datos.length() != 0){
+            etm.setText(preferences.getString(name, ""));
+        }else{
+            Toast.makeText(this,"NO hay registros con el nombre", Toast.LENGTH_SHORT).show();
+        }
     }
 }
