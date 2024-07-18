95dec803113d8b8a658b02cef4f76ebdb32331ab
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-18 15:00:57 -0500

    finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -13,13 +17,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button play;
+    SoundPool sp;
+    int sonido_de_Reproduccion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);*/
+        play = (Button)findViewById(R.id.button_play);
+        // Inicializar SoundPool
+        sp = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .build();
+
+        // Cargar el sonido
+        sonido_de_Reproduccion = sp.load(this, R.raw.sound_short, 1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,36 +40,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void Frutas(View view){
-        int id = view.getId();
-        if (id == R.id.bananas) {
-            Toast.makeText(this, "estas son bananas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.fresas) {
-            Toast.makeText(this, "estas son fresas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.cerezas) {
-            Toast.makeText(this, "estas son cerezas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.frambuesas) {
-            Toast.makeText(this, "estas son frambuesas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.kiwis) {
-            Toast.makeText(this, "estos son kiwis", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.mangos) {
-            Toast.makeText(this, "estos son mangos", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.manzanas) {
-            Toast.makeText(this, "estas son manzanas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.melon) {
-            Toast.makeText(this, "estos son melones", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.naranjas) {
-            Toast.makeText(this, "estas son naranjas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.peras) {
-            Toast.makeText(this, "estas son peras", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.pina) {
-            Toast.makeText(this, "estas son piñas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.sandia) {
-            Toast.makeText(this, "estas son sandias", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.uvas) {
-            Toast.makeText(this, "estas son uvas", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.zarzamora) {
-            Toast.makeText(this, "estas son zarzamoras", Toast.LENGTH_SHORT).show();
+    public void AudioSoundPool(View view){
+        // Verificar si el sonido se ha cargado antes de reproducir
+        if (sonido_de_Reproduccion != 0) {
+            sp.play(sonido_de_Reproduccion, 1, 1, 1, 0, 1f);
+        } else {
+            Toast.makeText(this, "Error al cargar el sonido", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void AudiomediaPlayer(View view) {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sound_long);
+        mp.start();
     }
 }
