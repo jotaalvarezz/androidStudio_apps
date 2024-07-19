eba915856d43a74303824b38e4e23ddb99515833
jotaalvarezz <jotaalvarezz25@gmail.com>
2024-07-19 14:30:03 -0500

    reproductor finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,22 +18,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button play;
-    SoundPool sp;
-    int sonido_de_Reproduccion;
+    Button play, repeat;
+    ImageView iv;
+    MediaPlayer mp;
+    int sonido_de_Reproduccion, repetir = 2, position = 0;
+    MediaPlayer vectormp [] = new MediaPlayer[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        play = (Button)findViewById(R.id.button_play);
-        // Inicializar SoundPool
-        sp = new SoundPool.Builder()
-                .setMaxStreams(1)
-                .build();
+        play = (Button)findViewById(R.id.btn_play);
+        repeat = (Button)findViewById(R.id.btn_repeat);
+        iv = (ImageView)findViewById(R.id.imageView);
 
-        // Cargar el sonido
-        sonido_de_Reproduccion = sp.load(this, R.raw.sound_short, 1);
+        vectormp[0] = MediaPlayer.create(this, R.raw.race);
+        vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+        vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -40,17 +43,117 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void AudioSoundPool(View view){
-        // Verificar si el sonido se ha cargado antes de reproducir
-        if (sonido_de_Reproduccion != 0) {
-            sp.play(sonido_de_Reproduccion, 1, 1, 1, 0, 1f);
-        } else {
-            Toast.makeText(this, "Error al cargar el sonido", Toast.LENGTH_SHORT).show();
+    public void PlayPause(View view){
+      if(vectormp[position].isPlaying()){
+          vectormp[position].pause();
+          play.setBackgroundResource(R.drawable.reproducir);
+          Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+      } else {
+          vectormp[position].start();
+          play.setBackgroundResource(R.drawable.pausa);
+          Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+      }
+    }
+
+    public void Stop(View view) {
+        if (vectormp[position] != null){
+            //stop vacea la posicion actual
+            vectormp[position].stop();
+            //por lo tanto se debe indicar despues las canciones nuevamente
+            vectormp[0] = MediaPlayer.create(this, R.raw.race);
+            vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+            vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+            position = 0;
+            play.setBackgroundResource(R.drawable.reproducir);
+            iv.setImageResource(R.drawable.portada1);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void AudiomediaPlayer(View view) {
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.sound_long);
-        mp.start();
+    public void Repeat(View view){
+        if(repetir == 1){
+            repeat.setBackgroundResource(R.drawable.no_repetir);
+            Toast.makeText(this, "No Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[position].setLooping(false);
+            repetir = 2;
+        }else{
+            repeat.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[position].setLooping(true);
+            repetir = 1;
+        }
+    }
+
+    public void Next(View view){
+        if(position < vectormp.length - 1){
+            if(vectormp[position].isPlaying()){
+                vectormp[position].stop();
+                position++;
+                vectormp[position].start();
+                switch (position){
+                    case 0:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 1:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+            }else{
+                position++;
+                switch (position){
+                    case 0:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 1:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+            }
+        } else {
+            Toast.makeText(this, "No hay Canciones", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Back(View view){
+        if(position >= 1){
+            if(vectormp[position].isPlaying()){
+                vectormp[position].stop();
+                vectormp[0] = MediaPlayer.create(this, R.raw.race);
+                vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+                vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+                position--;
+                vectormp[position].start();
+                switch (position){
+                    case 0:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 1:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+            }else{
+                position--;
+                switch (position){
+                    case 0:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 1:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+            }
+        }
     }
 }
